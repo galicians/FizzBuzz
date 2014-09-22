b40b49f20d15ba5e2960197132fa439a9418df8d
galicians <galicians@gmail.com>
2014-09-22 13:08:08 +0100

    Fizzbuzz tests are included

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,19 +3,36 @@ require 'FizzBuzz'
 describe FizzBuzz do
 	let(:fizzbuzz) { FizzBuzz.new }
 
-	it "knows when a number is divisible by 3" do
-		expect(fizzbuzz.is_divisible_by_three?(3)).to be true
-	end
+	context 'knows when a number is divisible by' do
+		it "3" do
+			expect(fizzbuzz.is_divisible_by_three?(3)).to be true
+		end
 
-	it "knows when a number is divisible by 5" do
-		expect(fizzbuzz.is_divisible_by_five?(5)).to be true
-	end
+		it "5" do
+			expect(fizzbuzz.is_divisible_by_five?(5)).to be true
+		end
 
-	it "knows when a number is divisible by 15" do
-		expect(fizzbuzz.is_divisible_by_fifteen?(15)).to be true
+		it "15" do
+			expect(fizzbuzz.is_divisible_by_fifteen?(15)).to be true
+		end
 	end
 
 	it 'works only with numbers' do
 		expect{fizzbuzz.is_divisible_by_three?('3')}.to raise_error("RuntimeError")
 	end
+
+	context 'shoud say' do
+
+		it "Fizz when a number is divisible by 3" do
+			expect(fizzbuzz.say(3)).to eq('Fizz')
+		end
+
+		it "Buzz when a number is divisible by 5" do
+			expect(fizzbuzz.say(5)).to eq('Buzz')
+		end
+
+		it "FizzBuzz when a number is divisible by 15" do
+			expect(fizzbuzz.say(15)).to eq('FizzBuzz')
+		end
+	end	
 end
