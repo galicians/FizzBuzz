89f7771539cb4792e75d39d9a1b71257d244a301
galicians <galicians@gmail.com>
2014-09-22 12:24:25 +0100

    logic added to divisible 3 & 5 & 15

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,8 +1,15 @@
 class FizzBuzz
 
 	def is_divisible_by_three?(number)
-		true
+		number % 3 == 0
 	end
 
+	def is_divisible_by_five?(number)
+		number % 5 == 0
+	end
+
+	def is_divisible_by_fifteen?(number)
+		number % 15 == 0
+	end
 end
 
